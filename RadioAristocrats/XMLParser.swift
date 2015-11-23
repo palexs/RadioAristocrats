0f735f799c65e5d0eb80d322e1a999f8a5bee574
Oleksandr Perepelitsyn <Oleksandr.Perepelitsyn@tomtom.com>
2015-11-23 18:09:50 +0200

    Refactors XMLParser class to use SWXMLHash framework and new xmls.

---
+++ RadioAristocrats/XMLParser.swift
@@ -0,0 +1,33 @@
+//
+//  HtmlParser.swift
+//  RadioAristocrats
+//
+//  Created by Oleksandr Perepelitsyn on 3/6/15.
+//  Copyright (c) 2015 RadioAristocrats. All rights reserved.
+//
+
+import Foundation
+import SWXMLHash
+
+class XMLParser {
+    
+    class func parse(xmlString: String, channel: RadioManager.ChannelType) -> Track? {
+        let xml = SWXMLHash.parse(xmlString)
+        var title: String?
+        var artist: String?
+        
+        switch channel {
+            case .Stream:
+                title = xml["Schedule"]["Event"]["Song"].element?.attributes["title"]
+                artist = xml["Schedule"]["Event"]["Song"]["Artist"].element?.attributes["name"]
+            
+            case .AMusic, .Jazz:
+                title = xml["Playlist"]["song"].element?.attributes["title"]
+                artist = xml["Playlist"]["artist"].element?.attributes["title"]
+        }
+        
+        let track = Track(title:title!, artist:artist!)
+        return track
+    }
+    
+}
