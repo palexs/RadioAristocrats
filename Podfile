62b3e99990b84d27f6f20c8b379414abeb786dfc
Oleksandr Perepelitsyn <Oleksandr.Perepelitsyn@tomtom.com>
2015-11-23 16:52:27 +0200

    Adds SWXMLHash framework.

--- Podfile
+++ Podfile
@@ -4,3 +4,4 @@ source 'https://github.com/CocoaPods/Specs.git'
 use_frameworks!
 pod 'ReachabilitySwift', git: 'https://github.com/ashleymills/Reachability.swift'
 pod 'Alamofire', '~> 3.0'
+pod 'SWXMLHash', '~> 2.0.0'
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,8 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/ReachabilitySwift.framework"
+  install_framework "Pods/SWXMLHash.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/ReachabilitySwift.framework"
+  install_framework "Pods/SWXMLHash.framework"
 fi
